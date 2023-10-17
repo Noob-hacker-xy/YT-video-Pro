267d8861e1bf38f8c2167dec5f2d1626ebae5fcb
Noob-hacker-xy <114892015+Noob-hacker-xy@users.noreply.github.com>
2023-10-17 11:42:26 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+## Introduction
+
+ytpro is a script for playing and downloading youtube videos/audios from termux.
+
+## Features
+
+1. Play youtube audio without download.
+2. Run audio in background.
+3. Download videos in 720p and 1080p
+4. Download audio in best available quality.
+5. Download audio in mp3
+6. comming soon..:)
+
+## Installation:- 
+
+```
+# update && upgrade termux
+pkg upgrade -y
+# install git
+pkg install git -y
+# clone the tool
+git clone https://github.com/BDhaCkers009/ytpro.git
+
+# navigate to the tools folder
+cd ytpro
+
+# run the installer script
+
+bash tuo-install.sh
+
+```
+```
+
+# or 
+chmod +x tuo-install.sh && ./tuo-install.sh
+```
+## Usage:-
+
+Its usage is very simple. 
+Just open _YouTube app_  **(not in any browser)** then press share button of your desired video then select **TERMUX** then you will see 2 options:-  
+
+1. Play audio ( without download)
+2. Download video (with yt-dlp)
+3. Download only audio (bestaudio quality)
+4. Download only audio (mp3)
+
+Now if you want to listen the audio then select 1 and press enter it will play the audio. But if you want to download the video then select 2 and press enter, after pressing enter you will see resolution select as your wish.If you want to download only audio then select 3 or 4.
+
+After downloading the  audio or video you can find it on your **Phone Storage** there you will find **YouTube** named folder and inside that folder you will find the downloaded audio or video. 
+
+
+### Special Thanks:
+- [**Tahmid Rayat (HTR-TECH)**](https://github.com/htr-tech)
+
+## Note:-
+
+This is in beta stage.
+
+
+
+### Thanks for using my repo :)
---
+++ tuo-install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+starts() {
+	clear
+	if [[ ! -d "/data/data/com.termux/files/home" ]]; then
+		echo "
+		
+██╗░░██╗░█████╗░██████╗░██╗███╗░░░███╗
+██║░██╔╝██╔══██╗██╔══██╗██║████╗░████║
+█████═╝░██║░░██║██████╔╝██║██╔████╔██║
+██╔═██╗░██║░░██║██╔══██╗██║██║╚██╔╝██║
+██║░╚██╗╚█████╔╝██║░░██║██║██║░╚═╝░██║
+╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝
+		
+		"
+		
+		
+		
+		echo -e "This script is only for TERMUX Use\n"
+		echo -e "Please wait for another Update\n"
+		exit 0
+	fi
+
+	if [[ ! -d "$HOME/storage" ]]; then
+		echo -e "Setting up Storage\n"
+		echo -e "Please Allow Storage Permission\n"
+		sleep 2
+		termux-setup-storage
+	fi
+}
+
+install_pkgs() {
+	pkgs=(python mpv ffmpeg)
+
+	for i in "${pkgs[@]}"; do
+		if [[ ! $(command -v "$i") ]]; then
+			type -p "$i" &>/dev/null || {
+				echo -e "\nInstalling Please w8 $i\n"
+				pkg install -y "$i"
+				sleep 1
+			}
+		fi
+	done
+
+	if [[ ! $(command -v pip3) ]]; then
+		curl -k --progress-bar -L "https://bootstrap.pypa.io/get-pip.py" -o "gpip.py"
+		python gpip.py
+		rm gpip.py
+	fi
+	if [[ ! $(command -v yt-dlp) ]]; then
+		pip3 install yt-dlp
+	fi
+}
+
+config_s() {
+	directory=("$HOME/bin" "$HOME/.config/yt-dlp" "/sdcard/YouTube video dounload by korim")
+	for d in "${directory[@]}"; do
+		if [[ ! -d "$d" ]]; then
+			echo "Creating Directory : $d"
+			mkdir -p $d
+		fi
+	done
+
+	confs=("$HOME/bin/termux-url-opener" "$HOME/.config/yt-dlp/config")
+	for c in "${confs[@]}"; do
+		if [[ -e "$c" ]]; then
+			echo "Backing Up : $c"
+			mv $c "${c}.bak"
+		fi
+		if [[ -e $(basename $c) ]]; then
+			cp $(basename $c) "$c"
+			chmod 777 "$c"
+		else
+			echo -e "\nSome Config files not found,,,Clone Again\n"
+			exit 0
+		fi
+	done
+	
+	echo -e "\nConfiguration Completed !!\n"
+}
+
+starts
+install_pkgs
+config_s
